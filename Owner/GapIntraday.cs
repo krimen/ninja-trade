3558d4bf777868c1055a657b5a1ec1ab3847675e
Gabriel Rodrigues <x.gabrielrodrigues.x@gmail.com>
2024-06-18 10:35:09 -0300

    Updated start of reactagle for when gaps was opened

--- Owner/GapIntraday.cs
+++ Owner/GapIntraday.cs
@@ -27,7 +27,7 @@ namespace NinjaTrader.NinjaScript.Indicators.Customs
 	[Gui.CategoryOrder("Date", 1)]
 	[Gui.CategoryOrder("Colors", 2)]
 	public class GapIntraday : Indicator
-	{	
+	{
 		private static List<NinjaTrader.Custom.Indicators.Customs.Gap> gaps;
 		private int twoPreviousBarIndex = 2;
 		private int widthRect = -50;
@@ -37,32 +37,32 @@ namespace NinjaTrader.NinjaScript.Indicators.Customs
 		{
 			if (State == State.SetDefaults)
 			{
-				Description									= @"Micro gaps between bars";
-				Name										= "GapIntraday";
-				Calculate									= Calculate.OnBarClose;
-				IsOverlay									= true;
-				DisplayInDataBox							= true;
-				DrawOnPricePanel							= true;
-				DrawHorizontalGridLines						= true;
-				DrawVerticalGridLines						= true;
-				PaintPriceMarkers							= true;
-				ScaleJustification							= NinjaTrader.Gui.Chart.ScaleJustification.Right;
+				Description = @"Micro gaps between bars";
+				Name = "GapIntraday";
+				Calculate = Calculate.OnBarClose;
+				IsOverlay = true;
+				DisplayInDataBox = true;
+				DrawOnPricePanel = true;
+				DrawHorizontalGridLines = true;
+				DrawVerticalGridLines = true;
+				PaintPriceMarkers = true;
+				ScaleJustification = NinjaTrader.Gui.Chart.ScaleJustification.Right;
 				//Disable this property if your indicator requires custom values that cumulate with each new market data event. 
 				//See Help Guide for additional information.
-				IsSuspendedWhileInactive					= true;
-				gaps 										= new List<NinjaTrader.Custom.Indicators.Customs.Gap>();
-				LastDaySession 								= DateTime.Now;
-				GapBullColor 								= Brushes.LightSkyBlue;
-				GapBearColor    							= Brushes.Red;
-				Opacity 									= 13;
+				IsSuspendedWhileInactive = true;
+				gaps = new List<NinjaTrader.Custom.Indicators.Customs.Gap>();
+				LastDaySession = DateTime.Now;
+				GapBullColor = Brushes.LightSkyBlue;
+				GapBearColor = Brushes.Red;
+				Opacity = 13;
 				PrintTo = PrintTo.OutputTab1;
 
 			}
 			else if (State == State.Configure)
 			{
-				
+
 			}
-	
+
 		}
 
 		private NinjaTrader.Custom.Indicators.Customs.MyBar GetMyBar(int position = 0)
@@ -72,7 +72,7 @@ namespace NinjaTrader.NinjaScript.Indicators.Customs
 
 		private void CheckFilled()
 		{
-			
+
 			List<NinjaTrader.Custom.Indicators.Customs.Gap> filled = new List<NinjaTrader.Custom.Indicators.Customs.Gap>();
 			int currentBarIndex = 0;
 			NinjaTrader.Custom.Indicators.Customs.MyBar currentBar = this.GetMyBar(currentBarIndex);
@@ -81,33 +81,33 @@ namespace NinjaTrader.NinjaScript.Indicators.Customs
 
 			foreach (NinjaTrader.Custom.Indicators.Customs.Gap gap in gapsNotFilled)
 			{
-				
+
 				if (gap is NinjaTrader.Custom.Indicators.Customs.BearGap)
 				{
 					// if gap was closed
-					if(currentBar.Close >= gap.Box.UpperPrice)
-                    {
+					if (currentBar.Close >= gap.Box.UpperPrice)
+					{
 						gap.State = NinjaTrader.Custom.Indicators.Customs.FillType.Closed;
 						filled.Add(gap);
 					}
 					// if negative gap but not closed
 					else if (currentBar.Close < gap.Box.UpperPrice && currentBar.High > gap.Box.LowerPrice)
-                    {
+					{
 						int indexGapOnTheList = gaps.FindIndex(item => item.Tag == gap.Tag);
 						bool founded = indexGapOnTheList != -1;
-						if(founded)
-                        {
+						if (founded)
+						{
 							gap.Box.LowerPrice = currentBar.High;
 							gap.State = Custom.Indicators.Customs.FillType.Negative;
 							gaps[indexGapOnTheList] = gap;
-                        }
+						}
 					}
 
 				}
 				else if (gap is NinjaTrader.Custom.Indicators.Customs.BullGap)
 				{
 					// if gap was closed
-					if(currentBar.Close <= gap.Box.LowerPrice)
+					if (currentBar.Close <= gap.Box.LowerPrice)
 					{
 						gap.State = NinjaTrader.Custom.Indicators.Customs.FillType.Closed;
 						filled.Add(gap);
@@ -116,65 +116,52 @@ namespace NinjaTrader.NinjaScript.Indicators.Customs
 					{
 						int indexGapOnTheList = gaps.FindIndex(item => item.Tag == gap.Tag);
 						bool founded = indexGapOnTheList != -1;
-						if(founded)
-                        {
+						if (founded)
+						{
 							gap.Box.UpperPrice = currentBar.Low;
 							gap.State = Custom.Indicators.Customs.FillType.Negative;
 							gaps[indexGapOnTheList] = gap;
-                        }
+						}
 					}
 				}
-//				if(gap.Closed(currentBar.Close))
-//				{
-//					gap.State = NinjaTrader.Custom.Indicators.Customs.FillType.Closed;
-//					filled.Add(gap);
-//				}
-//				else if (gap.NegativeGap(currentBar))
-//				{
-//					gap.UpdateBox(currentBar);
-//					int indexGapOnTheList = gaps.FindIndex(item => item.Tag == gap.Tag);
-//					bool founded = indexGapOnTheList != -1;
-//					gaps[indexGapOnTheList] = gap;
-//				}
 
 			}
-			
+
 
 			this.RemoveGaps(filled);
-			
+
 			List<Custom.Indicators.Customs.Gap> gapsNegatives = gaps.Where(gap => gap.State == Custom.Indicators.Customs.FillType.Negative).ToList();
 			this.UpdateGaps(gapsNegatives);
-			
+
 		}
-		
+
 		private void UpdateGaps(List<Custom.Indicators.Customs.Gap> negatives)
 		{
-			foreach(var negative in negatives)
+			foreach (var negative in negatives)
 			{
-				string tag = string.Empty;
-			    negative.State = NinjaTrader.Custom.Indicators.Customs.FillType.Open;
+				negative.State = NinjaTrader.Custom.Indicators.Customs.FillType.Open;
 
-			    if (negative is NinjaTrader.Custom.Indicators.Customs.BullGap)
-			    {
-			        Draw.Rectangle(this, negative.Tag, false, twoPreviousBarIndex, negative.Box.UpperPrice, negative.Box.Width, negative.Box.LowerPrice, negative.Box.BorderColor, negative.Box.BackgroundColor, negative.Box.Opacity, negative.Box.OnPricePanel); ;
+				int offSetBar = (_barCntr - negative.NumberBar) + 2; 
 
-			    }
-			    else
-			    {
-			        Draw.Rectangle(this, negative.Tag, false, twoPreviousBarIndex, negative.Box.LowerPrice, negative.Box.Width, negative.Box.UpperPrice, negative.Box.BorderColor, negative.Box.BackgroundColor, negative.Box.Opacity, negative.Box.OnPricePanel);
-			    }
+				if (negative is NinjaTrader.Custom.Indicators.Customs.BullGap)
+				{
+					Draw.Rectangle(this, negative.Tag, false, offSetBar, negative.Box.UpperPrice, negative.Box.Width, negative.Box.LowerPrice, negative.Box.BorderColor, negative.Box.BackgroundColor, negative.Box.Opacity, negative.Box.OnPricePanel); ;
+
+				}
+				else
+				{
+					Draw.Rectangle(this, negative.Tag, false, offSetBar, negative.Box.LowerPrice, negative.Box.Width, negative.Box.UpperPrice, negative.Box.BorderColor, negative.Box.BackgroundColor, negative.Box.Opacity, negative.Box.OnPricePanel);
+				}
 			}
 		}
 
 		private void RemoveGaps(List<Custom.Indicators.Customs.Gap> gapsToRemove)
-        {
+		{
 			gapsToRemove.ForEach(gap =>
 			{
 				if (DrawObjects[gap.Tag] != null)
 				{
 					var drawObject = DrawObjects[gap.Tag];
-					Rectangle rect = (Rectangle)drawObject;
-
 					RemoveDrawObject(gap.Tag);
 				}
 
@@ -184,70 +171,74 @@ namespace NinjaTrader.NinjaScript.Indicators.Customs
 
 		protected override void OnBarUpdate()
 		{
-			
+
 			DateTime lastSession = LastDaySession;
-			if (Time[0].Day == lastSession.Day) 
+			if (Time[0].Day == lastSession.Day)
 			{
-				if(Bars.IsFirstBarOfSession)
+				if (Bars.IsFirstBarOfSession)
 				{
-					_barCntr = 1;	
+					_barCntr = 1;
 				}
-				else 
+				else
 				{
 					++_barCntr;
 					//Print("Barra (" + _barCntr + ") das : " + Time[0].ToString());
 				}
-				
+
 				if (_barCntr < 2) return;
-			
+
 				NinjaTrader.Custom.Indicators.Customs.MyBar currentBar = this.GetMyBar();
 				NinjaTrader.Custom.Indicators.Customs.MyBar twoPreviousBar = this.GetMyBar(twoPreviousBarIndex);
-				
-				
-				
-				if(currentBar.Low > twoPreviousBar.High)
+
+
+
+				if (currentBar.Low > twoPreviousBar.High)
 				{
-	                string tag = "gapup" + CurrentBar;
-	                NinjaTrader.Custom.Indicators.Customs.Gap gap =
-	                    new NinjaTrader.Custom.Indicators.Customs.BullGap(tag, twoPreviousBar.High, currentBar.Low);
+					string tag = "gapup" + _barCntr;
+					NinjaTrader.Custom.Indicators.Customs.Gap gap =
+						new NinjaTrader.Custom.Indicators.Customs.BullGap(tag, twoPreviousBar.High, currentBar.Low);
 
 					gap.Box.Width = widthRect;
-					gap.Box.BorderColor		= Brushes.Transparent;
+					gap.Box.BorderColor = Brushes.Transparent;
 					gap.Box.BackgroundColor = GapBullColor;
-					gap.Box.Opacity			= Opacity;
+					gap.Box.Opacity = Opacity;
+					gap.NumberBar = _barCntr;
+
 
 
 					gaps.Add(gap);
 					Draw.Rectangle(this, tag, false, twoPreviousBarIndex, currentBar.Low, gap.Box.Width, twoPreviousBar.High, gap.Box.BorderColor, gap.Box.BackgroundColor, gap.Box.Opacity, gap.Box.OnPricePanel);
 				}
-	
-	            else if (currentBar.High < twoPreviousBar.Low)
-				{
-	                string tag = "gapdown" + CurrentBar;
-	    
-	                NinjaTrader.Custom.Indicators.Customs.Gap gap =
-	                    new NinjaTrader.Custom.Indicators.Customs.BearGap(tag, currentBar.High, twoPreviousBar.Low);
 
-					gap.Box.Width			= widthRect;
-					gap.Box.BorderColor		= Brushes.Transparent;
+				else if (currentBar.High < twoPreviousBar.Low)
+				{
+					string tag = "gapdown" + _barCntr;
+
+					NinjaTrader.Custom.Indicators.Customs.Gap gap =
+						new NinjaTrader.Custom.Indicators.Customs.BearGap(tag, currentBar.High, twoPreviousBar.Low);
+
+					gap.Box.Width = widthRect;
+					gap.Box.BorderColor = Brushes.Transparent;
 					gap.Box.BackgroundColor = GapBearColor;
-					gap.Box.Opacity			= Opacity;
+					gap.Box.Opacity = Opacity;
+					gap.NumberBar = _barCntr;
 					gaps.Add(gap);
+
 
 					Draw.Rectangle(this, tag, false, twoPreviousBarIndex, twoPreviousBar.Low, gap.Box.Width, currentBar.High, gap.Box.BorderColor, gap.Box.BackgroundColor, gap.Box.Opacity, gap.Box.OnPricePanel);
 				}
-				
-				
+
+
 				CheckFilled();
 			}
-			
+
 		}
-	
-		
+
+
 		//checks for gap between current candle and 2 previous candle e.g. low of current candle and high of the candle before last, this is the fair value gap.
-		private void GapLogic(NinjaTrader.Custom.Indicators.Customs.MyBar currentCandle, NinjaTrader.Custom.Indicators.Customs.MyBar twoPreviosCandle) 
+		private void GapLogic(NinjaTrader.Custom.Indicators.Customs.MyBar currentCandle, NinjaTrader.Custom.Indicators.Customs.MyBar twoPreviosCandle)
 		{
-			if(currentCandle is NinjaTrader.Custom.Indicators.Customs.BullBar)
+			if (currentCandle is NinjaTrader.Custom.Indicators.Customs.BullBar)
 			{
 				if (currentCandle.High - twoPreviosCandle.Low < 0)
 				{
@@ -256,7 +247,7 @@ namespace NinjaTrader.NinjaScript.Indicators.Customs
 					double midlimit = (upperLimit + lowerLimit) / 2;
 				}
 			}
-			else 
+			else
 			{
 				if (currentCandle.Low - twoPreviosCandle.High > 0)
 				{
@@ -265,49 +256,49 @@ namespace NinjaTrader.NinjaScript.Indicators.Customs
 					double midlimit = (upperLimit + lowerLimit) / 2;
 				}
 			}
-				
+
 		}
 
 		#region Properties
-		
+
 		[NinjaScriptProperty]
 		[XmlIgnore]
-		[Display(Name="Last Day Session", Description = "Last Day Session ", Order=1, GroupName= "Date")]
+		[Display(Name = "Last Day Session", Description = "Last Day Session ", Order = 1, GroupName = "Date")]
 		public DateTime LastDaySession
 		{
 			get;
-			set; 
+			set;
 		}
-		
+
 		[NinjaScriptProperty]
 		[XmlIgnore]
-		[Display(Name="Gap Bull Color", Description = "Gap for bull bars", Order=2, GroupName= "Colors")]
+		[Display(Name = "Gap Bull Color", Description = "Gap for bull bars", Order = 2, GroupName = "Colors")]
 		public Brush GapBullColor
 		{
 			get;
-			set; 
+			set;
 		}
-		
+
 		[NinjaScriptProperty]
 		[XmlIgnore]
-		[Display(Name="Gap Bear Color", Description = "Gap for bull bars", Order=3, GroupName= "Colors")]
+		[Display(Name = "Gap Bear Color", Description = "Gap for bull bars", Order = 3, GroupName = "Colors")]
 		public Brush GapBearColor
 		{
 			get;
-			set; 
+			set;
 		}
-		
+
 		[NinjaScriptProperty]
 		[XmlIgnore]
-		[Display(Name="Opacity", Description = "Opacity for Gap Color", Order=4, GroupName= "Colors")]
-		[Range(0,100)]
+		[Display(Name = "Opacity", Description = "Opacity for Gap Color", Order = 4, GroupName = "Colors")]
+		[Range(0, 100)]
 		public byte Opacity
 		{
 			get;
-			set; 
+			set;
 		}
-		
-	#endregion
+
+		#endregion
 	}
 
 }
